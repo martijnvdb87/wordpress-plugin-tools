49c37f5cdf81ae038647c0f31061f6e75bfdd27e
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2021-03-27 17:39:38 +0100

    Added license information

--- src/Template.php
+++ src/Template.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * This file is part of the martijnvdb/wordpress-plugin-tools library
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @copyright Copyright (c) Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
+ * @license http://opensource.org/licenses/MIT MIT
+ */
+
 namespace Martijnvdb\WordpressPluginTools;
 
 // Prevent direct access
